d47c0df1525aafcc8428b98046bcb32d7c060e8a
bruno salas <salasbruno01@gmail.com>
2020-09-14 19:20:24 -0500

    bruno avance 1

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1f;
+
+    Vector3 velocity;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Vector3.zero;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(h, 0f, v) * speed * Time.deltaTime;
+
+        rb.MovePosition(playerMovement + transform.position);
+
+    }
+}
