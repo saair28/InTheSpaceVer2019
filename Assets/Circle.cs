e67f464d120b1114685fa0a0abd29d4d81a33138
Adrian <adrianibarsam@gmail.com>
2020-10-05 18:53:47 -0500

    Version 2019 fc adrian 5/10/2020

---
+++ Assets/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Circle : MonoBehaviour
+{
+    [SerializeField] Image CircleImg;
+    //[SerializeField] Text txtProgress;
+
+    [SerializeField] [Range(0, 1)] float progress = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            progress = progress + 0.001f;
+            CircleImg.fillAmount = progress;
+            
+        }
+    }
+}
