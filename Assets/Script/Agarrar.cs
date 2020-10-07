1a1982175b39808fb1815435666e44867550b822
bruno salas <salasbruno01@gmail.com>
2020-10-07 18:21:36 -0500

    avance

---
+++ Assets/Script/Agarrar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrar : MonoBehaviour
+{
+    public bool Sujetar = true;
+
+    public static Agarrar instance;
+
+    public bool Entrar = false;
+
+    public CabinaOxigeno cabina;
+
+    public void Start()
+    {
+        instance = this;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sujetar")
+        {
+            other.GetComponentInParent<Player>().Manos = this.gameObject;
+
+            Entrar = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Sujetar")
+        {
+            other.GetComponentInParent<Player>().Manos = null;
+
+            Entrar = false;
+        }
+    }
+}
---
+++ Assets/Script/PosicionOxigeno.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosicionOxigeno : MonoBehaviour
+{
+    public bool Sujetar = true;
+
+    public static PosicionOxigeno instance;
+
+    public bool Entrar = false;
+
+    public CabinaOxigeno cabina;
+
+    public void Start()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        cabina = CabinaOxigeno.instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ponlo")
+        {
+            cabina.GetComponent<CabinaOxigeno>().Posicion = this.gameObject;
+
+            Entrar = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ponlo")
+        {
+
+            cabina.GetComponent<CabinaOxigeno>().Posicion = null;
+
+            Entrar = false;
+        }
+    }
+}
